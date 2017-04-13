1c1c078fd927abd06fc93fe2a6fe205c7e3d3a32
Joel Esquilin <jjoelesquilin@gmail.com>
2017-04-13 17:59:35 -0400

    Made Dial, Adjusted Hand Trigger size, and turned on head collider

---
+++ Assets/DialBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialBehavior : MonoBehaviour {
+
+	ButtonFunctions machine;
+	// Use this for initialization
+	void Start () {
+		machine = GameController.Instance.GetComponent<ButtonFunctions> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (transform.localRotation.y > 0) {
+            machine.fireRate = 1;
+		} else if (transform.localRotation.y < 0) {
+            machine.fireRate = 3;
+		} else {
+            machine.fireRate = 2;
+		}
+	}
+}
